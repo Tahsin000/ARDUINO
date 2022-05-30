9cedd2906239c2465fb552d2720ee69141425059
Tahsin000 <59111015+Tahsin000@users.noreply.github.com>
2022-05-31 00:58:52 +0600

    Update Code.ino

--- EGG INCUBATOR/Code/Code/Code.ino
+++ EGG INCUBATOR/Code/Code/Code.ino
@@ -1,15 +1,19 @@
 #include "DHT.h"
 #include <LiquidCrystal.h>
 #define dht_1 2
-#define DHTTYPE DHT11
-LiquidCrystal lcd (12, 11, 7, 6, 5, 4);
-
+#define DHTTYPE DHT22
+int Contrast=75;
+LiquidCrystal lcd(12, 11, 7, 5, 4, 3);
 DHT dht(dht_1, DHTTYPE);
 int FAN = 13;
+int light = 8;
+volatile byte relayState = LOW;
 void setup() {
   pinMode(FAN, OUTPUT);
-  pinMode(8, OUTPUT);
+  pinMode(light, OUTPUT);
   Serial.begin(9600);
+  analogWrite(6,Contrast);
+  lcd.begin(16, 2);
   dht.begin();
 }
 
@@ -18,19 +22,32 @@ void loop() {
 
   float humid = dht.readHumidity();
   float temp = dht.readTemperature();
-  if (humid <= 60)digitalWrite(FAN, LOW);
-  if (humid > 60)digitalWrite(FAN, HIGH);
-  if (temp < 30) digitalWrite(8, HIGH);
-  if (temp >= 30) digitalWrite(8, LOW);
-  lcd.begin(16,  2);
+  if (humid <= 50 )digitalWrite(FAN, LOW);
+  if (humid > 60)digitalWrite(FAN, HIGH); 
+  if (temp >= 38)
+  {
+    if(temp >= 40){
+      digitalWrite(light, LOW);
+    }
+    else{
+      digitalWrite(light, HIGH);
+    }
+  }
+  if (temp < 34)
+  {
+    if(temp < 32){
+      digitalWrite(light, HIGH);
+    }
+    else{
+      digitalWrite(light, LOW);
+    }
+  } 
+  
+  
+  lcd.setCursor(0, 0);
   lcd.print("Humidity : ");
   lcd.print(humid);
   lcd.setCursor(0, 1);
-  lcd.print("Temperature : ");
+  lcd.print("Temp : ");
   lcd.print(temp);
-//  Serial.println("Humidity : ");
-//  Serial.println(humid);
-//  Serial.println("Temperature : ");
-//  Serial.println(temp);
-//  Serial.println("Degree centigrade : ");
 }

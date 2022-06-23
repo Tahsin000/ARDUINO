d4ca9b683a6a4fff9e03b74500e7b2e264af64bb
Tahsin000 <59111015+Tahsin000@users.noreply.github.com>
2022-06-23 09:26:15 +0600

    FInal_CODE_real_LIFE_project

--- EGG INCUBATOR/Code/Code/Code.ino
+++ EGG INCUBATOR/Code/Code/Code.ino
@@ -2,52 +2,59 @@
 #include <LiquidCrystal.h>
 #define dht_1 2
 #define DHTTYPE DHT22
-int Contrast=75;
+int Contrast = 75;
 LiquidCrystal lcd(12, 11, 7, 5, 4, 3);
 DHT dht(dht_1, DHTTYPE);
 int FAN = 13;
 int light = 8;
 volatile byte relayState = LOW;
-void setup() {
-  pinMode(FAN, OUTPUT);
-  pinMode(light, OUTPUT);
-  Serial.begin(9600);
-  analogWrite(6,Contrast);
-  lcd.begin(16, 2);
-  dht.begin();
+void setup()
+{
+    pinMode(FAN, OUTPUT);
+    pinMode(light, OUTPUT);
+    Serial.begin(9600);
+    analogWrite(6, Contrast);
+    lcd.begin(16, 2);
+    dht.begin();
 }
 
-void loop() {
-  delay(250);
+void loop()
+{
+    delay(250);
 
-  float humid = dht.readHumidity();
-  float temp = dht.readTemperature();
-  if (humid <= 50 )digitalWrite(FAN, LOW);
-  if (humid > 60)digitalWrite(FAN, HIGH); 
-  if (temp >= 38)
-  {
-    if(temp >= 40){
-      digitalWrite(light, LOW);
+    float humid = dht.readHumidity();
+    float temp = dht.readTemperature();
+    if (humid <= 50)
+        digitalWrite(FAN, LOW);
+    if (humid > 60)
+        digitalWrite(FAN, HIGH);
+    if (temp >= 38)
+    {
+        if (temp >= 40)
+        {
+            digitalWrite(light, LOW);
+        }
+        else
+        {
+            digitalWrite(light, HIGH);
+        }
     }
-    else{
-      digitalWrite(light, HIGH);
+    if (temp < 34)
+    {
+        if (temp < 32)
+        {
+            digitalWrite(light, HIGH);
+        }
+        else
+        {
+            digitalWrite(light, LOW);
+        }
     }
-  }
-  if (temp < 34)
-  {
-    if(temp < 32){
-      digitalWrite(light, HIGH);
-    }
-    else{
-      digitalWrite(light, LOW);
-    }
-  } 
-  
-  
-  lcd.setCursor(0, 0);
-  lcd.print("Humidity : ");
-  lcd.print(humid);
-  lcd.setCursor(0, 1);
-  lcd.print("Temp : ");
-  lcd.print(temp);
+
+    lcd.setCursor(0, 0);
+    lcd.print("Humidity : ");
+    lcd.print(humid);
+    lcd.setCursor(0, 1);
+    lcd.print("Temp : ");
+    lcd.print(temp);
 }
